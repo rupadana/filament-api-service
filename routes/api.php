0f71175ad899337b7a557dd0bb47d8e7c56c470c
I Wayan Rupadana <rupadanawayan@gmail.com>
2024-02-09 21:23:38 +0800

    public api

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')
     ->name('api.')
-    ->middleware('auth:sanctum')
     ->group(function () {
         $panels = Filament::getPanels();
 
