ff3eb00e2c49677ff4283071ec1696d9e5d28c78
Eelco <eelcoschreurs@gmail.com>
2024-10-20 16:31:11 +0200

    allow for no panel prefix when only one panel in Filament is used.

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,12 @@ use Rupadana\ApiService\Exceptions\InvalidTenancyConfiguration;
 Route::prefix('api')
     ->name('api.')
     ->group(function () {
-        if (ApiService::tenancyAwareness() && (! ApiService::isRoutePrefixedByPanel() || ! ApiService::isTenancyEnabled())) {
-            throw new InvalidTenancyConfiguration("Tenancy awareness is enabled!. Please set 'api-service.route.panel_prefix=true' and 'api-service.tenancy.enabled=true'");
-        }
 
         $panels = Filament::getPanels();
+
+        if ((ApiService::tenancyAwareness() && count($panels) > 1) && (! ApiService::isRoutePrefixedByPanel() || ! ApiService::isTenancyEnabled())) {
+            throw new InvalidTenancyConfiguration("Tenancy awareness is enabled!. Please set 'api-service.route.panel_prefix=true' and 'api-service.tenancy.enabled=true'");
+        }
 
         foreach ($panels as $key => $panel) {
             try {
@@ -27,10 +28,9 @@ Route::prefix('api')
 
                 if (
                     $hasTenancy &&
-                    ApiService::isTenancyEnabled() &&
-                    ApiService::tenancyAwareness()
+                    ApiService::isTenancyEnabled()
                 ) {
-                    $routePrefix = $panelRoutePrefix . (($tenantRoutePrefix) ? "{$tenantRoutePrefix}/" : '') . '{tenant' . (($tenantSlugAttribute) ? ":{$tenantSlugAttribute}" : '') . '}/';
+                    $routePrefix = $panelRoutePrefix . ((ApiService::tenancyAwareness() && $tenantRoutePrefix) ? "{$tenantRoutePrefix}/" : '') . '{tenant' . (($tenantSlugAttribute) ? ":{$tenantSlugAttribute}" : '') . '}/';
                     Route::name($panelNamePrefix)
                         ->middleware($middlewares)
                         ->group(function () use ($panel, $routePrefix, $apiServicePlugin) {
