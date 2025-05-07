637e08940ea73a98c24c4addb2065ab1060349d0
Rupadana <34137674+rupadana@users.noreply.github.com>
2025-05-07 13:43:58 +0800

    Update AuthController.php

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
 
         return response()->json(
             [
-                'sucess' => false,
+                'success' => false,
                 'message' => 'The provided credentials are incorrect.',
             ],
             401
